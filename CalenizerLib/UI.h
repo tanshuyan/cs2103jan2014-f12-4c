6640a8bf9d772a3639695094ac1261684665c942
des <dquek2@gmail.com>
2014-03-13 23:13:16 +0800

    temp ui

---
+++ CalenizerLib/UI.h
@@ -0,0 +1,49 @@
+//UI.h
+// for testing purposes
+#pragma once
+#ifndef UI_H
+#define UI_H
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include "TaskStorage.h"
+#include "Task.h"
+#include "Logic.h"
+
+class UI {
+private:
+	
+
+	enum COMMAND_TYPE {
+		ADD, ADDD, ADDT, DELETE, SEARCH, SORT, EXIT, INVALID, EDIT, EDITD, EDITT, COMPLETE, DISPLAY, UNDO, REDO
+	};
+
+	static const std::string CMD_ADD;
+	static const std::string CMD_EDIT;
+	static const std::string CMD_DELETE;
+	static const std::string CMD_DISPLAY;
+	static const std::string CMD_EXIT;
+	static const std::string CMD_SEARCH;
+	static const std::string CMD_COMPLETE;
+	static const std::string CMD_EDITT;
+	static const std::string CMD_EDITD;
+	static const std::string CMD_ADDT;
+	static const std::string CMD_ADDD;
+	static const std::string CMD_UNDO;
+	static const std::string CMD_REDO;
+	static const std::string CMD_USERPROMPT;
+
+	std::istringstream _inputStream;
+	Logic _cmdOperation;
+
+public:
+	UI();
+	void ProgramLaunch();
+	COMMAND_TYPE determineCommand(std::string);
+	void executeCommand(std::string);
+
+};
+
+#endif
