22389cde9fc2891c0f00d82b371816bbe1065a7e
des <dquek2@gmail.com>
2014-03-13 23:10:04 +0800

    test main driver

---
+++ QTCalenizer/main.cpp
@@ -0,0 +1,15 @@
+
+#include <QtCore/QCoreApplication>
+#include <iostream>
+#include <string>
+#include "Logic.h"
+#include "TaskStorage.h"
+#include "UI.h"
+
+int main(int argc, char *argv[])
+{
+	UI test;
+	test.ProgramLaunch();
+	QCoreApplication a(argc, argv);
+	return a.exec();
+}
