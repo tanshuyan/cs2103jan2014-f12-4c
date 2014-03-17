e79210ad4e20ef29f7394b0e88c5ef03be7df316
Monika <Monika@localhost>
2014-03-17 21:53:15 +0800

    Monika Added GUI Add a simple GUI for testing purposes, takes input from Parser and display output from the Parser function. I am using a displayCMD as test function.

---
+++ CalenizerGUI/calenizergui.h
@@ -0,0 +1,23 @@
+#ifndef CALENIZERGUI_H
+#define CALENIZERGUI_H
+
+#include <QtWidgets/QMainWindow>
+#include "ui_calenizergui.h"
+#include "Parser.h"
+
+class CalenizerGUI : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	CalenizerGUI(QWidget *parent = 0);
+	~CalenizerGUI();
+
+public slots: void on_pushButton_released();
+
+private:
+	Ui::CalenizerGUIClass ui;
+	Parser _g_parser;
+};
+
+#endif // CALENIZERGUI_H
