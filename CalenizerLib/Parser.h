e66c0770176eb86d6379ced5dd96b894447e19ae
tanshuyan <tanshuyan@localhost>
2014-03-21 01:17:29 +0800

    Tested and assured to be in working condition Major Bug Fix: Fixed bug with recognition of "starting/beginning from" Other minor bug fixes as well
    Now more badass than ever

--- CalenizerLib/Parser.h
+++ CalenizerLib/Parser.h
@@ -1,11 +1,11 @@
 //Parser.h
 //IN PROGRESS
-//v 1.2
+//v 1.4
 
 #ifndef PARSER_H
 #define PARSER_H
 
-#include "Logic.h"
+#include "LogicStub.h"
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -14,7 +14,7 @@
 #include <vector>
 #include <algorithm>
 #include <QRegExp>
-#include "DateTimeParser.h"
+#include "NLParser.h"
 
 enum TaskType{
 	ACTIVITY, DEADLINE, EVENT, FLOATINGTASK
@@ -27,6 +27,8 @@ private:
 	//std::string _userInput;
 	std::vector<int> _validIndex;
 	std::vector<int> _invalidIndex;
+
+	NLParser _nlParser;
 	
 	std::vector<std::string> _taskDesc;
 	// _startDate ;
@@ -73,14 +75,6 @@ private:
 	static const std::string KEYWORD_ON;
 	static const std::string KEYWORD_BY;
 	static const std::string KEYWORD_AT;
-	
-	// this are keywords for edit
-	static QRegExp RX_FROM_UNTIL;
-	static QRegExp RX_FROM;
-	//Not yet implemented
-	static QRegExp RX_UNTIL;
-	static QRegExp RX_ON_AT_BY;
-	static QRegExp RX_UNTIL_FROM;
 
 
 public:
@@ -91,17 +85,12 @@ public:
 	std::string toLower(std::string);
 
 	COMMAND_TYPE getCommand(std::string);
-
-
-	std::string addCMD(std::string);
 	
 	bool isTimedTask(std::string);
 	bool isDeadlineTask(std::string);
 	
-	
-	
-	std::string editCMD(std::string);
-	
+	std::string editCMD(std::string userInput);
+	std::string addCMD(std::string userInput);
 	
 	//Able to delete multiple indexes 
 	std::string deleteCMD(std::string);
@@ -122,5 +111,6 @@ public:
 	void getValidIndex(std::string);
 	bool isValidIndex(int index);
 	int convertToIndex(std::string); 
+
 };
 #endif
