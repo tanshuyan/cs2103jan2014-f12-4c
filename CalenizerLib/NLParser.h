bb53d95c26e989c9555f929e3e3cbd447feff5d0
tanshuyan <tanshuyan@localhost>
2014-03-23 09:32:54 +0800

    Parser V2.2
    Parser now can recognise Today, Tomorrow, the day after, and their respective
    short forms and common misspellings.
    Parser can now recognise dates in the format of 3rd dec, 1st july, etc.

---
+++ CalenizerLib/NLParser.h
@@ -0,0 +1,32 @@
+//NLParser.h
+//v 2.2
+
+#ifndef NLPARSER_H
+#define NLPARSER_H
+
+#include <QDateTime>
+#include <QString>
+#include <string>
+#include <QRegExp>
+#include "DateTimeParser.h"
+
+//All functions return false if any error occurs
+class NLParser{
+private:
+	DateTimeParser _dateTimeParser;
+	
+	static QRegExp RX_FROM_UNTIL;
+	static QRegExp RX_UNTIL_FROM;
+	static QRegExp RX_ON_UNTIL;
+	static QRegExp RX_START;
+	static QRegExp RX_END;
+	static QRegExp RX_ON_AT_BY;
+	static QRegExp RX_FROM;
+
+public:
+	NLParser();
+	void parse(QString &descString, QDate &startDate, QTime &startTime, QDate &endDate, QTime &endTime, bool &dateTimeIsUnlablled);
+	//Does not alter descString
+	void guessContextualTime(QString descString, QTime &startTime);
+};
+#endif
---
+++ CalenizerLib/UIDisplay.h
@@ -0,0 +1,62 @@
+//UIDisplay.h
+
+#pragma once
+#ifndef UIDISPLAY_H
+#define UIDISPLAY_H
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include "taskStorage.h"
+#include "task.h"
+
+class UIDisplay {
+
+private:
+
+	static const std::string MSG_WELCOME;
+	static const std::string MSG_USERPROMPT;
+	static const std::string MSG_ADD_SUCCESS;
+	static const std::string MSG_EDIT_SUCCESS;
+	static const std::string MSG_DELETE_SUCCESS;
+	static const std::string MSG_UNDO_SUCCESS;
+	static const std::string MSG_REDO_SUCCESS;
+	static const std::string MSG_COMPLETE_SUCCESS;
+
+	static const std::string MSG_INVALID_CMD;
+	static const std::string MSG_INVALID_LINENO;
+	static const std::string MSG_INVALID_TIME;
+	static const std::string MSG_INVALID_DATE;
+
+	static const std::string MSG_NOT_FOUND;
+	static const std::string MSG_EMPTY_FILE;
+	static const std::string DELIMITER;
+	static const int MAX_SIZE = 255;
+
+	char buffer[MAX_SIZE];
+	static const std::string _programName;
+
+public:
+	UIDisplay();
+	
+	void displayTasks(std::vector<Task*>);
+
+	std::string welcomeMsg();
+	std::string clearFeedback();
+	std::string emptyFeedback();
+	std::string editFeedback(std::string);
+	std::string addFeedback(std::string);
+	std::string deleteFeedback(std::string);
+	std::string undoFeedback();
+	std::string redoFeedback();
+	std::string notFoundFeedback(std::string);
+	std::string invalidFeedback();
+	std::string invalidTimeFeedback();
+	std::string invalidDateFeedback();
+	std::string completeFeedback(std::string);
+
+	void displayUI(std::string output);
+};
+
+#endif
