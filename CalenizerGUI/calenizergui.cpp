b924015618a4f2963e8541c099c92c769ced6565
Yi Kai <yk.kwah@gmail.com>
2014-03-25 22:31:10 +0800

    Changed GUI (v1.1)

--- CalenizerGUI/calenizergui.cpp
+++ CalenizerGUI/calenizergui.cpp
@@ -4,12 +4,13 @@ CalenizerGUI::CalenizerGUI(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
-	//QPalette* palette = new QPalette();
-	//palette->setColor(QPalette::WindowText,Qt::white);
-	//ui.label->setPalette(*palette);
-	//QLineEdit *line = new QLineEdit();
-	//palette->setColor(QPalette::Text,Qt::white);
-	//ui.lineEdit->setPalette(*palette);
+	QPalette* palette; 
+	palette = new QPalette();
+	palette->setColor(QPalette::WindowText,Qt::black);
+	ui.label->setPalette(*palette);
+	QLineEdit *line = new QLineEdit();
+	palette->setColor(QPalette::Text,Qt::black);
+	ui.lineEdit->setPalette(*palette);
 }
 
 CalenizerGUI::~CalenizerGUI()
--- CalenizerGUI/main.cpp
+++ CalenizerGUI/main.cpp
@@ -1,13 +1,20 @@
 #include "calenizergui.h"
 #include <QtWidgets/QApplication>
+#include <QSplashScreen>
+#include "Windows.h"
+
 // v1.0
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-	CalenizerGUI w;
-	//w.setStyleSheet("background-color: #A52A2A ");
-	w.show();
-	return a.exec();
+	QApplication app(argc, argv);
+	
+	QPixmap pixmap(":/CalenizerGUI/Resources/hc.jpg");
+	QSplashScreen splash(pixmap, Qt::WindowStaysOnTopHint);
+	splash.show();
+	Sleep(300);
+
+	CalenizerGUI calenizer;
+	calenizer.setStyleSheet("background-color: #ffffff");
+	calenizer.show();
+	return app.exec();
 }
-
-
