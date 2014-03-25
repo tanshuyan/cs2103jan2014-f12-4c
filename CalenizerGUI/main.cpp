8afb8140117077057256f03d03a1622f03a4588e
Yi Kai <yk.kwah@gmail.com>
2014-03-25 15:25:07 +0800

    fixed problem of GUI not loading. Now can load.

--- CalenizerGUI/main.cpp
+++ CalenizerGUI/main.cpp
@@ -5,13 +5,9 @@ int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	CalenizerGUI w;
-	w.setStyleSheet("background-color: #A52A2A ");
+	//w.setStyleSheet("background-color: #A52A2A ");
 	w.show();
 	return a.exec();
 }
 
-void CalenizerGUI::on_lineEdit_returnPressed()
-{
-	ui.textEdit->setTextColor(Qt::white);
-	ui.textEdit->append(QString::fromStdString(_g_parser.displayCMD(ui.lineEdit->text().toStdString())));	
-}
+
