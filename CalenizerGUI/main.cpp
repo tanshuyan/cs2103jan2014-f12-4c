a1df64568d72dfb3cf9b1f1d30ffe2ff3ec76af3
Monika <Monika@localhost>
2014-03-27 22:58:23 +0800

    updated main for GUI

--- CalenizerGUI/main.cpp
+++ CalenizerGUI/main.cpp
@@ -1,17 +1,29 @@
 #include "calenizergui.h"
 #include <QtWidgets/QApplication>
-// v1.0
+
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	CalenizerGUI w;
-	w.setStyleSheet("background-color: #A52A2A ");
+	w.setStyleSheet("background-color: gray ");
 	w.show();
 	return a.exec();
 }
 
 void CalenizerGUI::on_lineEdit_returnPressed()
 {
+	DisplayOutput displayoutput = _g_logic.executeUserInput(ui.lineEdit->text().toStdString());
 	ui.textEdit->setTextColor(Qt::white);
-	ui.textEdit->append(QString::fromStdString(_g_parser.displayCMD(ui.lineEdit->text().toStdString())));	
+	ui.textEdit->append(QString::fromStdString(displayoutput.getFeedBack()));
+
+	ui.tableWidget->setRowCount(displayoutput.getDisplay().size());
+	for(int row=0; row<displayoutput.getDisplay().size(); row++){
+		std::string initialString = displayoutput.getDisplay().at(row);
+		int startIndex = initialString.find_first_of("<>");
+		int lastIndex = initialString.find_last_of("<>");
+		std::string taskDesc = initialString.substr(startIndex+2,lastIndex-(startIndex+3));
+		std::string duration = initialString.substr(lastIndex+1);
+		ui.tableWidget->setItem(row, 0, new QTableWidgetItem(QString::fromStdString(taskDesc)));
+		ui.tableWidget->setItem(row, 1, new QTableWidgetItem(QString::fromStdString(duration)));
+	}
 }
