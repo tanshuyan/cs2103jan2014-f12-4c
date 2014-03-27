04f7bf17b8fa5d9fb999d97b000dd8401bd18078
Yi Kai <yk.kwah@gmail.com>
2014-03-27 20:27:45 +0800

    V0.2 made changes to DisplayOutput Rid of magic strings.

--- CalenizerLib/DisplayOutput.cpp
+++ CalenizerLib/DisplayOutput.cpp
@@ -69,5 +69,129 @@ std::string DisplayOutput::promptMsg(){
 	return buffer;
 }
 
+const std::string DisplayOutput::MSG_ADD_SUCCESS ="added to %s: \"%s\"";
+const std::string DisplayOutput::MSG_EDIT_SUCCESS ="%s updated";
+const std::string DisplayOutput::MSG_DELETE_SUCCESS ="task deleted from %s";
+const std::string DisplayOutput::MSG_UNDO_SUCCESS ="latest change to %s undone";
+const std::string DisplayOutput::MSG_UNDO_FAILURE ="Nothing to undone in %s";
+const std::string DisplayOutput::MSG_REDO_SUCCESS ="latest undo to %s reverted";
+const std::string DisplayOutput::MSG_REDO_FAILURE ="Nothing to redo in %s";
+const std::string DisplayOutput::MSG_SEARCH_SUCCESS ="search term %s found in %s";
+const std::string DisplayOutput::MSG_SEARCH_FAILURE ="search term %s not found in %s";
+const std::string DisplayOutput::MSG_COMPLETE_SUCCESS ="%S marked as complete";
+
+const std::string DisplayOutput::MSG_DISPLAYCOM = "completed tasks in %s shown";
+const std::string DisplayOutput::MSG_DISPLAYINCOM = "incompleted tasks in %s shown";
+const std::string DisplayOutput::MSG_DISPLAYTODAY = "today tasks in %s shown";
+const std::string DisplayOutput::MSG_DISPLAYALL = "all tasks in %s shown";
+
+const std::string DisplayOutput::MSG_INVALID_CMD = "invalid command";
+const std::string DisplayOutput::MSG_INVALID_INDEX = "invalid index";
+const std::string DisplayOutput::MSG_INVALID_TIME = "invalid time";
+const std::string DisplayOutput::MSG_INVALID_DATE = "invalid date";
+
+const std::string DisplayOutput::MSG_NOT_FOUND ="\"%s\" not found in %s";
+const std::string DisplayOutput::MSG_EMPTY_FILE = "%s is empty";
+
+std::string DisplayOutput::emptyFeedback() {
+	sprintf_s(buffer, MSG_EMPTY_FILE.c_str(), _programName.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::deleteFeedback() {
+	sprintf_s(buffer, MSG_DELETE_SUCCESS.c_str(), _programName.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::addFeedback(std::string userInput) {
+	sprintf_s(buffer, MSG_ADD_SUCCESS.c_str(), _programName.c_str(), userInput.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::invalidFeedback(){
+	sprintf_s(buffer, MSG_INVALID_CMD.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::notFoundFeedback(std::string userInput){
+	sprintf_s(buffer, MSG_NOT_FOUND.c_str(), userInput.c_str(), _programName.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::undoSuccessFeedback(){
+	sprintf_s(buffer, MSG_UNDO_SUCCESS.c_str(), _programName.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::undoFailureFeedback(){
+	sprintf_s(buffer, MSG_UNDO_FAILURE.c_str(), _programName.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::redoSuccessFeedback(){
+	sprintf_s(buffer, MSG_REDO_SUCCESS.c_str(), _programName.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::redoFailureFeedback(){
+	sprintf_s(buffer, MSG_REDO_FAILURE.c_str(), _programName.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::searchSuccessFeedback(std::string userInput){
+	sprintf_s(buffer, MSG_SEARCH_SUCCESS.c_str(), userInput.c_str(), _programName.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::searchFailureFeedback(std::string userInput){
+	sprintf_s(buffer, MSG_SEARCH_FAILURE.c_str(), userInput.c_str(),_programName.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::invalidIndexFeedback(){
+	sprintf_s(buffer, MSG_INVALID_INDEX.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::invalidTimeFeedback(){
+	sprintf_s(buffer, MSG_INVALID_TIME.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::invalidDateFeedback(){
+	sprintf_s(buffer, MSG_INVALID_DATE.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::completeFeedback(std::string userInput){
+	sprintf_s(buffer,  MSG_COMPLETE_SUCCESS.c_str(), userInput.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::editFeedback(std::string userInput){
+	sprintf_s(buffer, MSG_EDIT_SUCCESS.c_str(), userInput.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::displayCompleteFeedback(){
+	sprintf_s(buffer, MSG_DISPLAYCOM.c_str(), _programName.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::displayIncompleteFeedback(){
+	sprintf_s(buffer, MSG_DISPLAYINCOM.c_str(), _programName.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::displayTodayFeedback(){
+	sprintf_s(buffer, MSG_DISPLAYTODAY.c_str(), _programName.c_str());
+	return buffer;
+}
+
+std::string DisplayOutput::displayAllFeedback(){
+	sprintf_s(buffer, MSG_DISPLAYALL.c_str(), _programName.c_str());
+	return buffer;
+}
+
 const std::string DisplayOutput::MSG_WELCOME = "Welcome to %s. At your service.";
 const std::string DisplayOutput::MSG_USERPROMPT= "what would you like %s to do? Give your command: ";
--- CalenizerLib/DisplayOutput.h
+++ CalenizerLib/DisplayOutput.h
@@ -26,6 +26,35 @@ private:
 	std::vector<std::string> _displayList;
 	// keeps a corresponding tracker on the status of task, if it is overdue, completed, incompleted etc
 	std::vector<int> _displayTaskStatus;
+
+	static const std::string MSG_ADD_SUCCESS;
+	static const std::string MSG_EDIT_SUCCESS;
+	static const std::string MSG_DELETE_SUCCESS;
+
+	static const std::string MSG_UNDO_SUCCESS;
+	static const std::string MSG_UNDO_FAILURE;
+
+	static const std::string MSG_REDO_SUCCESS;
+	static const std::string MSG_REDO_FAILURE;
+
+	static const std::string MSG_SEARCH_SUCCESS;
+	static const std::string MSG_SEARCH_FAILURE;
+
+	static const std::string MSG_COMPLETE_SUCCESS;
+
+	static const std::string MSG_DISPLAYCOM;
+	static const std::string MSG_DISPLAYINCOM;
+	static const std::string MSG_DISPLAYTODAY;
+	static const std::string MSG_DISPLAYALL;
+
+	static const std::string MSG_INVALID_CMD;
+	static const std::string MSG_INVALID_INDEX;
+	static const std::string MSG_INVALID_TIME;
+	static const std::string MSG_INVALID_DATE;
+
+	static const std::string MSG_NOT_FOUND;
+	static const std::string MSG_EMPTY_FILE;
+
 public:
 	DisplayOutput();
 	void displayTasks(std::vector<Task*>);
@@ -37,6 +66,29 @@ public:
 	std::string welcomeMsg();
 	std::string promptMsg();
 	void displayToUser(std::string output);
+
+	std::string emptyFeedback();
+	std::string editFeedback(std::string);
+	std::string addFeedback(std::string);
+	std::string deleteFeedback();
+	std::string undoSuccessFeedback();
+	std::string undoFailureFeedback();
+	std::string redoSuccessFeedback();
+	std::string redoFailureFeedback();
+	std::string searchSuccessFeedback(std::string);
+	std::string searchFailureFeedback(std::string);
+	std::string completeFeedback(std::string);
+
+	std::string notFoundFeedback(std::string);
+	std::string invalidFeedback();
+	std::string invalidIndexFeedback();
+	std::string invalidTimeFeedback();
+	std::string invalidDateFeedback();
+
+	std::string displayCompleteFeedback();
+	std::string displayIncompleteFeedback();
+	std::string displayTodayFeedback();
+	std::string displayAllFeedback();
 
 };
 #endif
